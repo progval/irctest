83eaa42bb5a82a190b6f293dcfca1fcaec0fb67a
Valentin Lorentz <progval@progval.net>
2015-12-19 19:03:04 +0100

    Fix typo in setup.py.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3 :: Only'
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Communications :: Chat :: Internet Relay Chat',
         'Topic :: Software Development :: Testing',
         ],
