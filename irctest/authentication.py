37ea5be7539ad0e8d075bf8b8ea1b6139a84aa55
Valentin Lorentz <progval+git@progval.net>
2017-01-11 00:07:25 +0100

    Add tests for SCRAM.

--- irctest/authentication.py
+++ irctest/authentication.py
@@ -8,9 +8,11 @@ class Mechanisms(enum.Enum):
     def as_string(cls, mech):
         return {cls.plain: 'PLAIN',
                 cls.ecdsa_nist256p_challenge: 'ECDSA-NIST256P-CHALLENGE',
+                cls.scram_sha_256: 'SCRAM-SHA-256',
                 }[mech]
     plain = 1
     ecdsa_nist256p_challenge = 2
+    scram_sha_256 = 3
 
 Authentication = collections.namedtuple('Authentication',
         'mechanisms username password ecdsa_key')
