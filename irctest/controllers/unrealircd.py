c847db5420aeb453d0817d153b9d5f041a9cb943
Valentin Lorentz <progval+git@progval.net>
2021-07-07 19:26:58 +0200

    let's try strace debugging I guess

--- irctest/controllers/unrealircd.py
+++ irctest/controllers/unrealircd.py
@@ -168,6 +168,10 @@ class UnrealircdController(BaseServerController, DirectoryBasedController):
             )
         self.proc = subprocess.Popen(
             [
+                "strace",
+                "-e",
+                "file",
+                "-f",
                 "unrealircd",
                 "-t",
                 "-F",  # BOOT_NOFORK
