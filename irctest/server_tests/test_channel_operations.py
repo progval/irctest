2b66d8580c16e93ddc349730c01edfcf9f41dd28
Valentin Lorentz <progval+git@progval.net>
2021-08-07 14:30:33 +0200

    Test NAMES on invalid/nonexisting channel returns RPL_ENDOFNAMES.

--- irctest/server_tests/test_channel_operations.py
+++ irctest/server_tests/test_channel_operations.py
@@ -22,6 +22,7 @@ from irctest.numerics import (
     ERR_NOSUCHNICK,
     ERR_NOTONCHANNEL,
     ERR_UNKNOWNERROR,
+    RPL_ENDOFNAMES,
     RPL_INVITING,
     RPL_NAMREPLY,
     RPL_NOTOPIC,
@@ -1067,6 +1068,52 @@ class AuditoriumTestCase(cases.BaseServerTestCase):
         )
         self.assertEqual(
             len([msg for msg in self.getMessages("guest1") if msg.command == "QUIT"]), 0
+        )
+
+
+class NamesTestCase(cases.BaseServerTestCase):
+    @cases.mark_specifications("RFC1459", "RFC2812", "Modern")
+    def testNamesInvalidChannel(self):
+        """
+        "There is no error reply for bad channel names."
+        -- https://datatracker.ietf.org/doc/html/rfc1459#section-4.2.5
+        -- https://datatracker.ietf.org/doc/html/rfc2812#section-3.2.5
+
+        "If the channel name is invalid or the channel does not exist,
+        one `RPL_ENDOFNAMES` numeric containing the given channel name
+        should be returned."
+        -- https://modern.ircdocs.horse/#names-message
+        """
+        self.connectClient("foo")
+        self.getMessages(1)
+
+        self.sendLine(1, "NAMES invalid")
+        self.assertMessageMatch(
+            self.getMessage(1),
+            command=RPL_ENDOFNAMES,
+            params=["foo", "invalid", ANYSTR],
+        )
+
+    @cases.mark_specifications("RFC1459", "RFC2812", "Modern")
+    def testNamesNonexistingChannel(self):
+        """
+        "There is no error reply for bad channel names."
+        -- https://datatracker.ietf.org/doc/html/rfc1459#section-4.2.5
+        -- https://datatracker.ietf.org/doc/html/rfc2812#section-3.2.5
+
+        "If the channel name is invalid or the channel does not exist,
+        one `RPL_ENDOFNAMES` numeric containing the given channel name
+        should be returned."
+        -- https://modern.ircdocs.horse/#names-message
+        """
+        self.connectClient("foo")
+        self.getMessages(1)
+
+        self.sendLine(1, "NAMES #nonexisting")
+        self.assertMessageMatch(
+            self.getMessage(1),
+            command=RPL_ENDOFNAMES,
+            params=["foo", "#nonexisting", ANYSTR],
         )
 
 
