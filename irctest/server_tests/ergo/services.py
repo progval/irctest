00d2d42b12ec25d3ccdecc457dcec6d9cee341b8
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2022-01-10 16:45:50 -0500

    review fix

--- irctest/server_tests/ergo/services.py
+++ irctest/server_tests/ergo/services.py
@@ -1,6 +1,5 @@
 from irctest import cases
-from irctest.irc_utils.sasl import sasl_plain_blob
-from irctest.numerics import RPL_LOGGEDIN, RPL_SASLSUCCESS, RPL_YOUREOPER
+from irctest.numerics import RPL_YOUREOPER
 
 
 class NickservTestCase(cases.BaseServerTestCase):
@@ -17,16 +16,10 @@ class NickservTestCase(cases.BaseServerTestCase):
         self.getMessages("root")
 
         # test that the account was registered
-        self.addClient("saregister_test")
-        self.sendLine("saregister_test", "CAP REQ sasl")
-        self.sendLine("saregister_test", "NICK asdf")
-        self.sendLine("saregister_test", "USER u s e r")
-        self.sendLine("saregister_test", "AUTHENTICATE PLAIN")
-        self.sendLine(
-            "saregister_test",
-            sasl_plain_blob("saregister_test", "saregistertestpassphrase"),
+        self.connectClient(
+            name="saregister_test",
+            nick="saregister_test",
+            capabilities=["sasl"],
+            account="saregister_test",
+            password="saregistertestpassphrase",
         )
-        self.sendLine("saregister_test", "CAP END")
-        replies = {msg.command for msg in self.getMessages("saregister_test")}
-        self.assertIn(RPL_LOGGEDIN, replies)
-        self.assertIn(RPL_SASLSUCCESS, replies)
