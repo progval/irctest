5fe4d4cfd8ac90d8af40150f11974918772548e5
Valentin Lorentz <progval+git@progval.net>
2022-12-06 20:59:11 +0100

    github: Force ubuntu-20.04
    Bahamut does not support ubuntu-22.04

--- make_workflows.py
+++ make_workflows.py
@@ -116,7 +116,7 @@ def get_build_job(*, software_config, software_id, version_flavor):
         return None
 
     return {
-        "runs-on": "ubuntu-latest",
+        "runs-on": "ubuntu-20.04",
         "steps": [
             {
                 "name": "Create directories",
@@ -191,7 +191,7 @@ def get_test_job(*, config, test_config, test_id, version_flavor, jobs):
         unpack = []
 
     return {
-        "runs-on": "ubuntu-latest",
+        "runs-on": "ubuntu-20.04",
         "needs": needs,
         "steps": [
             {"uses": "actions/checkout@v2"},
@@ -307,7 +307,7 @@ def generate_workflow(config: dict, version_flavor: VersionFlavor):
     jobs["publish-test-results"] = {
         "name": "Publish Dashboard",
         "needs": sorted({f"test-{test_id}" for test_id in config["tests"]} & set(jobs)),
-        "runs-on": "ubuntu-latest",
+        "runs-on": "ubuntu-20.04",
         # the build-and-test job might be skipped, we don't need to run
         # this job then
         "if": "success() || failure()",
