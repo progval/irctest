0d5cf6111332195848efe559afa0bb5556731588
Val Lorentz <progval+git@progval.net>
2023-09-03 13:37:24 +0200

    WHO: Skip/xfail tests for Sable as needed

--- irctest/server_tests/who.py
+++ irctest/server_tests/who.py
@@ -87,7 +87,7 @@ class BaseWhoTestCase:
 class WhoTestCase(BaseWhoTestCase, cases.BaseServerTestCase):
     @cases.mark_specifications("Modern")
     def testWhoStar(self):
-        if self.controller.software_name == "Bahamut":
+        if self.controller.software_name in ("Bahamut", "Sable"):
             raise runner.OptionalExtensionNotSupported("WHO mask")
 
         self._init()
@@ -118,7 +118,7 @@ class WhoTestCase(BaseWhoTestCase, cases.BaseServerTestCase):
     )
     @cases.mark_specifications("Modern")
     def testWhoNick(self, mask):
-        if "*" in mask and self.controller.software_name == "Bahamut":
+        if "*" in mask and self.controller.software_name in ("Bahamut", "Sable"):
             raise runner.OptionalExtensionNotSupported("WHO mask")
 
         self._init()
@@ -148,7 +148,7 @@ class WhoTestCase(BaseWhoTestCase, cases.BaseServerTestCase):
         ids=["username", "realname-mask", "hostname"],
     )
     def testWhoUsernameRealName(self, mask):
-        if "*" in mask and self.controller.software_name == "Bahamut":
+        if "*" in mask and self.controller.software_name in ("Bahamut", "Sable"):
             raise runner.OptionalExtensionNotSupported("WHO mask")
 
         self._init()
@@ -201,7 +201,7 @@ class WhoTestCase(BaseWhoTestCase, cases.BaseServerTestCase):
     )
     @cases.mark_specifications("Modern")
     def testWhoNickAway(self, mask):
-        if "*" in mask and self.controller.software_name == "Bahamut":
+        if "*" in mask and self.controller.software_name in ("Bahamut", "Sable"):
             raise runner.OptionalExtensionNotSupported("WHO mask")
 
         self._init()
@@ -228,9 +228,14 @@ class WhoTestCase(BaseWhoTestCase, cases.BaseServerTestCase):
     @pytest.mark.parametrize(
         "mask", ["coolNick", "coolnick", "coolni*"], ids=["exact", "casefolded", "mask"]
     )
+    @cases.xfailIfSoftware(
+        ["Sable"],
+        "Sable does not advertise oper status in WHO: "
+        "https://github.com/Libera-Chat/sable/pull/77",
+    )
     @cases.mark_specifications("Modern")
     def testWhoNickOper(self, mask):
-        if "*" in mask and self.controller.software_name == "Bahamut":
+        if "*" in mask and self.controller.software_name in ("Bahamut", "Sable"):
             raise runner.OptionalExtensionNotSupported("WHO mask")
 
         self._init()
@@ -262,9 +267,14 @@ class WhoTestCase(BaseWhoTestCase, cases.BaseServerTestCase):
     @pytest.mark.parametrize(
         "mask", ["coolNick", "coolnick", "coolni*"], ids=["exact", "casefolded", "mask"]
     )
+    @cases.xfailIfSoftware(
+        ["Sable"],
+        "Sable does not advertise oper status in WHO: "
+        "https://github.com/Libera-Chat/sable/pull/77",
+    )
     @cases.mark_specifications("Modern")
     def testWhoNickAwayAndOper(self, mask):
-        if "*" in mask and self.controller.software_name == "Bahamut":
+        if "*" in mask and self.controller.software_name in ("Bahamut", "Sable"):
             raise runner.OptionalExtensionNotSupported("WHO mask")
 
         self._init()
@@ -298,7 +308,7 @@ class WhoTestCase(BaseWhoTestCase, cases.BaseServerTestCase):
     @pytest.mark.parametrize("mask", ["#chan", "#CHAN"], ids=["exact", "casefolded"])
     @cases.mark_specifications("Modern")
     def testWhoChan(self, mask):
-        if "*" in mask and self.controller.software_name == "Bahamut":
+        if "*" in mask and self.controller.software_name in ("Bahamut", "Sable"):
             raise runner.OptionalExtensionNotSupported("WHO mask")
 
         self._init()
@@ -582,7 +592,7 @@ class WhoServicesTestCase(BaseWhoTestCase, cases.BaseServerTestCase):
 class WhoInvisibleTestCase(cases.BaseServerTestCase):
     @cases.mark_specifications("Modern")
     def testWhoInvisible(self):
-        if self.controller.software_name == "Bahamut":
+        if self.controller.software_name in ("Bahamut", "Sable"):
             raise runner.OptionalExtensionNotSupported("WHO mask")
 
         self.connectClient("evan", name="evan")
