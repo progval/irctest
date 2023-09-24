44ab8af9da1e634d33f53d69644e83c2b8511bfc
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2023-09-23 20:51:03 -0400

    detect xdist, require filelock iff xdist

--- irctest/irc_utils/filelock.py
+++ irctest/irc_utils/filelock.py
@@ -4,16 +4,14 @@ commonly packaged by Linux distributions but might not be available
 in some environments.
 """
 
-try:
-    from filelock import FileLock
-except ImportError:
-    import contextlib
-    import warnings
+import os
 
-    warnings.warn(
-        "filelock package unavailable, concurrent runs may race",
-        RuntimeWarning,
-    )
+if os.getenv("PYTEST_XDIST_WORKER"):
+    # running under pytest-xdist; filelock is required for reliability
+    from filelock import FileLock
+else:
+    # normal test execution, no port races
+    import contextlib
 
     def FileLock(*args, **kwargs):
         return contextlib.nullcontext()
