737a3858d55c4701b7e096c60b72f08f748d1a53
Val Lorentz <progval+git@progval.net>
2024-04-12 11:47:05 +0200

    Add testNoMultiPrefix

--- irctest/server_tests/multi_prefix.py
+++ irctest/server_tests/multi_prefix.py
@@ -49,3 +49,50 @@ class MultiPrefixTestCase(cases.BaseServerTestCase):
                 msg=msg
             ),
         )
+
+    @cases.xfailIfSoftware(
+        ["irc2", "Bahamut"], "irc2 and Bahamut send a trailing space"
+    )
+    def testNoMultiPrefix(self):
+        """When not requested, only the highest prefix should be sent"""
+        self.connectClient("foo")
+        self.joinChannel(1, "#chan")
+        self.sendLine(1, "MODE #chan +v foo")
+        self.getMessages(1)
+
+        # TODO(dan): Make sure +v is voice
+
+        self.sendLine(1, "NAMES #chan")
+        reply = self.getMessage(1)
+        self.assertMessageMatch(
+            reply,
+            command="353",
+            params=["foo", ANYSTR, "#chan", "@foo"],
+            fail_msg="Expected NAMES response (353) with @foo, got: {msg}",
+        )
+        self.getMessages(1)
+
+        self.sendLine(1, "WHO #chan")
+        msg = self.getMessage(1)
+        self.assertEqual(
+            msg.command, "352", msg, fail_msg="Expected WHO response (352), got: {msg}"
+        )
+        self.assertGreaterEqual(
+            len(msg.params),
+            8,
+            "Expected WHO response (352) with 8 params, got: {msg}".format(msg=msg),
+        )
+        self.assertIn(
+            "@",
+            msg.params[6],
+            'Expected WHO response (352) with "@" in param 7, got: {msg}'.format(
+                msg=msg
+            ),
+        )
+        self.assertNotIn(
+            "+",
+            msg.params[6],
+            'Expected WHO response (352) with no "+" in param 7, got: {msg}'.format(
+                msg=msg
+            ),
+        )
