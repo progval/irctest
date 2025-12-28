9699d3e5680b962e42c60ec2db2ea068961e84ab
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2025-12-28 12:38:28 -0500

    enable ergo debug logging
    It gets suppressed anyway unless IRCTEST_DEBUG_LOGS=1 is set

--- irctest/controllers/ergo.py
+++ irctest/controllers/ergo.py
@@ -123,9 +123,8 @@ BASE_CONFIG = {
         "max-subs": 100,
         "max-keys": 1000,
     },
+    "logging": [{"method": "stderr", "level": "debug", "type": "*"}],
 }
-
-LOGGING_CONFIG = {"logging": [{"method": "stderr", "level": "debug", "type": "*"}]}
 
 
 def hash_password(password: Union[str, bytes]) -> str:
@@ -264,12 +263,6 @@ class ErgoController(BaseServerController, DirectoryBasedController):
     def getConfig(self) -> Dict:
         return copy.deepcopy(self._config)
 
-    def addLoggingToConfig(self, config: Optional[Dict] = None) -> Dict:
-        if config is None:
-            config = self.baseConfig()
-        config.update(LOGGING_CONFIG)
-        return config
-
     def addMysqlToConfig(self, config: Optional[Dict] = None) -> Dict:
         mysql_password = os.getenv("MYSQL_PASSWORD")
         if config is None:
@@ -307,11 +300,6 @@ class ErgoController(BaseServerController, DirectoryBasedController):
         case.getMessages(client)
         case.sendLine(client, "QUIT")
         case.assertDisconnected(client)
-
-    def enable_debug_logging(self, case: BaseServerTestCase) -> None:
-        config = self.getConfig()
-        config.update(LOGGING_CONFIG)
-        self.rehash(case, config)
 
 
 def get_irctest_controller_class() -> Type[ErgoController]:
