482fbc1af8db08b2ee6946d84160f58367254c68
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2025-12-29 03:11:02 -0500

    Update irctest/server_tests/regressions.py
    Co-authored-by: Val Lorentz <progval+github@progval.net>

--- irctest/server_tests/regressions.py
+++ irctest/server_tests/regressions.py
@@ -80,8 +80,8 @@ class RegressionsTestCase(cases.BaseServerTestCase):
         # bob messages alice so we can get a valid msgid for alice to reply to
         self.sendLine(2, "PRIVMSG alice :hey")
         self.getMessages(2)
-        msgs = self.getMessages(1)
-        bob_msgid = msgs[0].tags["msgid"]
+        msg, = self.getMessages(1)
+        bob_msgid = msg.tags["msgid"]
         self.assertNotEqual(bob_msgid, "")
 
         self.sendLine(1, f"@+draft/reply={bob_msgid} PRIVMSG bob :hey yourself")
