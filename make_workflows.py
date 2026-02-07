4632afc88afd66a45a106850fea3d2395cb0251b
Valentin Lorentz <progval+git@progval.net>
2026-02-07 23:46:21 +0100

    Explicitly set a password. and this requires a running postgres, so no need for wait-for-it anymore

--- make_workflows.py
+++ make_workflows.py
@@ -211,7 +211,7 @@ def get_test_job(*, config, test_config, test_id, version_flavor, jobs):
             *install_steps,
             {
                 "name": "Install system dependencies",
-                "run": "sudo apt-get install faketime wait-for-it",
+                "run": "sudo apt-get install faketime",
             },
             {
                 "name": "Install irctest dependencies",
